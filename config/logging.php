12b02270ce51d98da5e337031ce3023d730419bd
Plasteredpeak <hamza120025@gmail.com>
2026-04-09 22:24:22 +0100

    fix: update default log channel and emergency log path configuration

--- config/logging.php
+++ config/logging.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', env('APP_ENV') === 'production' ? 'stderr' : 'stack'),
 
     /*
     |--------------------------------------------------------------------------
@@ -98,7 +98,7 @@ return [
         ],
 
         'emergency' => [
-            'path' => storage_path('logs/laravel.log'),
+            'path' => env('LOG_EMERGENCY_PATH', '/tmp/laravel.log'),
         ],
     ],
 
